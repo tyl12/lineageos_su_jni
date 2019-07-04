c89b80d94cfb9b2f5b19f5a7b1944562778d740e
Teng Yiliang <ylteng@outlook.com>
2019-07-04 10:25:09 +0800

    init. based on lineageos commit: *********************************************************** *commit 11730a5 (HEAD, m/cm-14.1, github/cm-14.1) *Author: gwzhang <gwzhang@yahoo.com> *Date:   Tue Sep 12 18:59:06 2017 +0800 * *    correct delivering process return code to caller * *    Change-Id: I1adeaa2f6eaada2d1f3279515a5f32414046de2c ***********************************************************
    Signed-off-by: Teng Yiliang <ylteng@outlook.com>

---
+++ su_jni/Android.mk
@@ -0,0 +1,30 @@
+# Root AOSP source makefile
+# su is built here, and 
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := su
+LOCAL_MODULE_TAGS := optional
+LOCAL_WHOLE_STATIC_LIBRARIES := libcutils libutils libbinder liblog
+LOCAL_SRC_FILES := su.c daemon.c utils.c pts.c
+LOCAL_SRC_FILES += binder/appops-wrapper.cpp binder/pm-wrapper.c
+LOCAL_CFLAGS += -Werror -Wall
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
+LOCAL_INIT_RC := superuser.rc
+
+include $(BUILD_EXECUTABLE)
+
+SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
+$(SYMLINKS):
+	@echo "Symlink: $@ -> /system/xbin/su"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf ../xbin/su $@
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
+
